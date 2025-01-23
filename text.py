cdd153bb7c4ff9033c607fbe23ab0ff469d09bbf
MR. RANGO <74708667+RangoZEX@users.noreply.github.com>
2025-01-23 16:26:33 +0530

    Create text.py

---
+++ text.py
@@ -0,0 +1,6 @@
+MOVED_MSG = """
+📢 𝟑𝟎𝟏 𝐌𝐨𝐯𝐞𝐝 𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭𝐥𝐲
+**<blockquote>🚀 This bot has now permanently shifted to [UploadXPro](https://t.me/UploadXPro_Bot) for better features and an enhanced experience. Added Additional tools and features</blockquote>**\n\n
+**💗 [Thank you for your support!](https://t.me/maxxbotofficial)** 💙
+"""
+               
