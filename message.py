56749561d6ce7fa7e79a9df708b22cd5588b3151
MR. RANGO <74708667+RangoZEX@users.noreply.github.com>
2025-01-27 14:46:58 +0530

    Create message.py

---
+++ message.py
@@ -0,0 +1,11 @@
+REDIRECT_MSG = """
+📢 **301 Moved Permanently**
+<blockquote>
+**🚀 Important Update:**  
+This bot has permanently shifted to [UploadXPro](https://t.me/UploadXPro_Bot) for a better experience.  
+It now comes with **additional features** and tools for enhanced functionality.
+</blockquote>
+
+__Thank you for your continued support! 💗  
+Feel free to visit our official channel: [MaxxBot Official](https://t.me/maxxbotofficial) 💙__
+"""
